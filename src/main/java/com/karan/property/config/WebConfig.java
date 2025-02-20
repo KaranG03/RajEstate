ee6d7c4fb8350fc618097c98b732ad2a18724b6a
KaranG03 <137709248+KaranG03@users.noreply.github.com>
2025-02-20 13:33:16 +0530

    Update WebConfig.java
    sec config

--- src/main/java/com/karan/property/config/WebConfig.java
+++ src/main/java/com/karan/property/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://rajestate-production.up.railway.app/")
+                        .allowedOrigins("https://rajestate-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
